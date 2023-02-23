5f7df69b724bdb886759090a769a7387b1b87399
hyeonah <hyeonah.hello@gmail.com>
2023-02-23 16:57:09 -0500

    #5 feat: Display recipe detail information with layout

--- src/pages/RecipeDetailPage/index.js
+++ src/pages/RecipeDetailPage/index.js
@@ -1,3 +1,11 @@
+import {
+  Box,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from '@mui/material'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
@@ -54,7 +62,71 @@ const RecipeDetailPage = () => {
     console.log(recipeDetail)
   }, [recipeDetail])
 
-  return <div>RecipeDetailPage</div>
+  return (
+    <Box>
+      {Object.keys(recipeDetail).length && (
+        <Grid
+          container
+          spacing={2}
+          sx={{ border: '2px solid salmon' }}>
+          <Grid
+            item
+            xs={4}
+            sx={{ border: '1px solid green' }}>
+            <img
+              src={recipeDetail.image}
+              alt={recipeDetail.title}
+              loading='lazy'
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={8}
+            sx={{ border: '1px solid navy' }}>
+            <Typography>{recipeDetail.title}</Typography>
+            <Box>
+              <Typography>Health Information:</Typography>
+              <Typography>{recipeDetail.diets}</Typography>
+            </Box>
+            <Box>
+              <Typography>Ingredients:</Typography>
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '0.2rem' }}>
+                {recipeDetail.ingredients.map((item, index) => (
+                  <Box key={index}>
+                    <Box component='span'>{item[0]}</Box>
+                    <Box component='span'> {item[1].amount}</Box>
+                    <Box component='span'>
+                      {item[1].unitShort && ` ${item[1].unitShort}`}
+                    </Box>
+                    <Box component='span'>
+                      {recipeDetail.ingredients.length - 1 !== index && ','}
+                    </Box>
+                  </Box>
+                ))}
+              </Box>
+            </Box>
+            <Box>
+              <Typography>Instructions</Typography>
+              <List disablePadding>
+                {recipeDetail.instructions.map((item, index) => (
+                  <ListItem
+                    disablePadding
+                    key={index}
+                    sx={{ alignItems: 'flex-start', gap: '1rem' }}>
+                    <Typography sx={{ paddingTop: '0.2rem' }}>
+                      {index + 1}
+                    </Typography>
+                    <ListItemText primary={item} />
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
+          </Grid>
+        </Grid>
+      )}
+    </Box>
+  )
 }
 
 export default RecipeDetailPage
