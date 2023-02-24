0952a10a4fa576b7e4aba6e01f59ff58ec860635
hyeonah <hyeonah.hello@gmail.com>
2023-02-23 19:47:33 -0500

    #6 design: Move contents to Main comp and Apply design to homepage

---
+++ src/components/Main/index.js
@@ -0,0 +1,113 @@
+import { Box, Typography } from '@mui/material'
+import Grid from '@mui/material/Grid'
+import { Container } from '@mui/system'
+
+const Main = () => {
+  const serviceCopy = [
+    {
+      title: '🔎 Search it',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at justo mattis, faucibus mi id, tempus dui.',
+    },
+    {
+      title: '💡 Find it',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at justo mattis, faucibus mi id, tempus dui.',
+    },
+    {
+      title: '🧑‍🍳 Cook it',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at justo mattis, faucibus mi id, tempus dui.',
+    },
+  ]
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}>
+      <Container maxWidth='lg'>
+        <Grid
+          container
+          sx={{
+            height: '60vh',
+            alignItems: 'center',
+          }}>
+          <Grid
+            item
+            xs={6}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <img
+                src='https://user-images.githubusercontent.com/83247825/221052155-06f40bb4-e115-44e6-b496-32468528a063.png'
+                alt='brand character'
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                  maxWidth: '25rem',
+                }}
+              />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={6}>
+            <Box>
+              <Typography
+                sx={{
+                  fontSize: '5rem',
+                  fontWeight: 'bold',
+                  lineHeight: '1.2',
+                  marginBottom: '0.25em',
+                }}>
+                Welcome to
+                <br />
+                Recipe World
+              </Typography>
+              <Typography sx={{ fontSize: '2rem', fontWeight: 'light' }}>
+                Making healthier food choices is key
+                <br />
+                to a heart-healthy lifestyle
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+      <Box sx={{ backgroundColor: '#efefef' }}>
+        <Container maxWidth='lg'>
+          <Grid
+            container
+            sx={{
+              height: '30vh',
+              alignItems: 'center',
+            }}>
+            {serviceCopy.map((copy, index) => (
+              <Grid
+                item
+                xs={4}
+                key={index}
+                sx={{ padding: '0 1rem' }}>
+                <Typography
+                  sx={{
+                    fontSize: '2.5rem',
+                    fontWeight: 'light',
+                    marginBottom: '1rem',
+                  }}>
+                  {copy.title}
+                </Typography>
+                <Typography sx={{ fontWeight: 'light' }}>
+                  {copy.body}
+                </Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
+  )
+}
+
+export default Main
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,17 +2,20 @@ import {
   Box,
   Button,
   FormControl,
+  Grid,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
   OutlinedInput,
   Pagination,
-  Typography
+  Typography,
 } from '@mui/material'
-import ImageList from '@mui/material/ImageList'
-import ImageListItem from '@mui/material/ImageListItem'
-import ImageListItemBar from '@mui/material/ImageListItemBar'
+import { Container } from '@mui/system'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { useSearchRecipeMutation } from '../../api/requestApi'
+import Main from '../../components/Main'
 import { addRecipeItems } from '../../store/recipeItemsSlice'
 import { getTotalResults } from '../../store/totalResultsSlice'
 
@@ -71,95 +74,130 @@ const HomePage = () => {
   }
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}>
-      <Box
-        sx={{
-          width: '100%',
-          maxWidth: 500,
-        }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
-          <FormControl
-            size='small'
-            sx={{ width: '100%' }}>
-            <OutlinedInput
-              placeholder='Search your favourite food to get the recipe!'
-              autoFocus={true}
-              onChange={getInputValue}
-              onKeyPress={handleEnterOrClick}
-            />
-          </FormControl>
-          <Button
-            variant='contained'
-            size='medium'
-            onClick={handleEnterOrClick}
-            sx={{
-              boxShadow: 'none',
-              backgroundColor: '#2079ad',
-              '&:hover': {
-                boxShadow: 'none',
-                backgroundColor: '#2079ad',
-              },
-            }}>
-            Search
-          </Button>
-        </Box>
-      </Box>
-      <Box>
-        {totalResults ? (
-          <>
-            <Typography>
-              Found {totalResults} {keyword} recipes
-            </Typography>
-            <ImageList
-              cols={3}
-              gap={20}>
-              {recipeItems.map(item => (
-                <ImageListItem key={item.id}>
-                  <Link to={`/recipedetail/${item.id}`}>
-                    <img
-                      src={item.image}
-                      srcSet={item.image}
-                      alt={item.title}
-                      loading='lazy'
-                    />
-                    <ImageListItemBar
-                      title={item.title}
-                      sx={{
-                        '.MuiImageListItemBar-title': {
-                          whiteSpace: 'normal',
-                        },
-                      }}
-                    />
-                  </Link>
-                </ImageListItem>
-              ))}
-            </ImageList>
-            <Box
+    <>
+      <Box sx={{ backgroundColor: '#2079ad' }}>
+        <Container maxWidth='lg'>
+          <Grid container>
+            <Grid
+              item
+              xs={2}
+              sx={{
+                height: '10vh',
+                display: 'flex',
+                alignItems: 'center',
+              }}>
+              <Link to='/'>
+                <Box sx={{ padding: '2rem' }}>
+                  <img
+                    src='https://user-images.githubusercontent.com/83247825/221061838-cfe1822b-e346-4cb0-98ed-6082c85a11b2.png'
+                    alt='logo'
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'contain',
+                    }}
+                  />
+                </Box>
+              </Link>
+            </Grid>
+            <Grid
+              item
+              xs={10}
               sx={{
                 display: 'flex',
                 alignItems: 'center',
+                gap: '1rem',
+              }}>
+              <FormControl
+                size='small'
+                sx={{ width: '100%', backgroundColor: '#fff' }}>
+                <OutlinedInput
+                  placeholder='Search your favourite food to get the recipe!'
+                  autoFocus={true}
+                  onChange={getInputValue}
+                  onKeyPress={handleEnterOrClick}
+                />
+              </FormControl>
+              <Button
+                variant='contained'
+                size='medium'
+                onClick={handleEnterOrClick}
+                sx={{
+                  fontWeight: 'bold',
+                  boxShadow: 'none',
+                  backgroundColor: '#f9a849',
+                  '&:hover': {
+                    boxShadow: 'none',
+                    backgroundColor: '#f9a849',
+                  },
+                }}>
+                Search
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+      <Box
+        sx={{
+          height: '90vh',
+        }}>
+        {totalResults ? (
+          <Container maxWidth='lg'>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
                 justifyContent: 'center',
               }}>
-              <Pagination
-                count={count}
-                size='large'
-                page={page}
-                variant='outlined'
-                shape='rounded'
-                onChange={movePage}
-              />
+              <Typography>
+                Found {totalResults} {keyword} recipes
+              </Typography>
+              <ImageList
+                cols={3}
+                gap={20}>
+                {recipeItems.map(item => (
+                  <ImageListItem key={item.id}>
+                    <Link to={`/recipedetail/${item.id}`}>
+                      <img
+                        src={item.image}
+                        srcSet={item.image}
+                        alt={item.title}
+                        loading='lazy'
+                      />
+                      <ImageListItemBar
+                        title={item.title}
+                        sx={{
+                          '.MuiImageListItemBar-title': {
+                            whiteSpace: 'normal',
+                          },
+                        }}
+                      />
+                    </Link>
+                  </ImageListItem>
+                ))}
+              </ImageList>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Pagination
+                  count={count}
+                  size='large'
+                  page={page}
+                  variant='outlined'
+                  shape='rounded'
+                  onChange={movePage}
+                />
+              </Box>
             </Box>
-          </>
+          </Container>
         ) : (
-          <p>The search result will be displayed here</p>
+          <Main />
         )}
       </Box>
-    </Box>
+    </>
   )
 }
 
